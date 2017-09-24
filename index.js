35e5307e0253cf59d30fe87223333ca117a657f2
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2017-09-25 00:12:04 +0545

    Use end cursor to iterate over all the posts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { parseProfile, parsePosts } = require('./utils/parser')
 const { insertMany, insertOne } = require('./database/insert')
 const { insertObjects } = require('./algolia/insert')
 
-const USERNAME = 'shah_niti'
+const USERNAME = 'moonbao.np'
 const URL_PROFILE = `https://www.instagram.com/${USERNAME}/?__a=1`
 const URL_POSTS = 'https://www.instagram.com/graphql/query';
 
@@ -17,24 +17,41 @@ const URL_POSTS = 'https://www.instagram.com/graphql/query';
       json: true
     })
     const Profile = await insertOne(parseProfile(resProfile))
-    const resPosts = await http({
-      uri: URL_POSTS,
-      qs: {
-        query_id: '17888483320059182',
-        variables: JSON.stringify({
-          id: Profile.id,
-          first: 100
-        })
-      },
-      json: true
-    })
-    resPosts['name'] = Profile.name
-    resPosts['username'] = Profile.username
-    resPosts['profile_display'] = Profile.display
+    let hasNext = false
 
-    const Posts = parsePosts(resPosts)
-    const postWithObjectIDs = await insertMany(Posts)
-    const alogliaResult = await insertObjects(postWithObjectIDs)
+    const query = {
+      query_id: '17888483320059182',
+      variables: JSON.stringify({
+        id: Profile.id,
+        first: 100
+      })
+    }
+
+    do {
+      console.log('Next Query', query)
+      const resPosts = await http({
+        uri: URL_POSTS,
+        qs: query,
+        json: true
+      })
+      resPosts['name'] = Profile.name
+      resPosts['username'] = Profile.username
+      resPosts['profile_display'] = Profile.display
+
+      const Posts = parsePosts(resPosts)
+      const postWithObjectIDs = await insertMany(Posts)
+      const alogliaResult = await insertObjects(postWithObjectIDs)
+
+      const pageInfo = resPosts['data']['user']['edge_owner_to_timeline_media']['page_info']
+
+      hasNext = pageInfo['has_next_page']
+
+      const nextQuery = Function(`return ${query.variables}`)()
+      nextQuery.after = pageInfo['end_cursor']
+      query.variables = JSON.stringify(nextQuery)
+
+    } while (hasNext)
+
   } catch (e) {
     console.log("Error::", e)
   }
@@ -44,6 +61,9 @@ async function http (options = {}) {
   const defaults = {
     method: 'GET',
     uri: 'http://www.google.com',
+    headers: {
+      'Connection': 'close'
+    },
     resolveWithFullResponse: true
   }
   const opts = Object.assign({}, defaults, options)
